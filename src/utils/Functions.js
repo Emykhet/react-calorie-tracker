8df126e69b866a7ced704047ea76377fb20b6bf4
Emykhet <49447920+Emykhet@users.noreply.github.com>
2020-11-29 01:22:30 +0100

    .

--- src/utils/Functions.js
+++ src/utils/Functions.js
@@ -1,4 +1,8 @@
 import { v4 as uuidv4 } from "uuid";
+export const generateItemWithIdFunc = (model) =>{
+    const id= uuidv4()
+    return {id, ...model }
+}
 
 export const toggleStateByDispatchFunc = (method, state, actionType, ) =>{
     if(state){
@@ -7,21 +11,15 @@ export const toggleStateByDispatchFunc = (method, state, actionType, ) =>{
         method({payload: true, type: actionType})
       }
 }
-//_______________________________
+
 export const dispatchStateFunc = (method, payload, actionType) =>{
     method({payload: payload, type: actionType})
 }
 
-//_______________________________
 export const clearInputFunc = (input) =>{
     return input.current.value = ""
 }
-//_______________________________
-export const generateItemWithIdFunc = (model) =>{
-    const id= uuidv4()
-    return {id, ...model }
-}
-//_______________________________
+
 export const updateItemByIdFunc = (model, array) =>{
     const updatedMeals = array.map(arrayItem => {
         if(arrayItem.id === model.id) {
@@ -31,5 +29,6 @@ export const updateItemByIdFunc = (model, array) =>{
     }) 
     return updatedMeals
 }
-//_______________________________
+
+
 
